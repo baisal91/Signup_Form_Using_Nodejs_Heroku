609d0bf8362de19263c821872a776a31bc29b319
Baisalbek Urustanbekov <baisalbek4@gmail.com>
2020-10-20 20:15:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -36,11 +36,11 @@ app.post("/", function(req, res){
     };
 
     let jsonData = JSON.stringify(data);
-    const url = "https://us2.api.mailchimp.com/3.0/lists/8184f6f4c";
+    const url = "https://us2.api.mailchimp.com/3.0/lists/list-key";
 
     const options ={
         method: "POST",
-        auth: "baisal:5b2b25d16f02d22ecd70c2fb1e0789d-us2"
+        auth: "baisal:API-key"
     };
 
     
